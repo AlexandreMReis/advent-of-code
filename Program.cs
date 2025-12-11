1415e1162f8bf8001276a28b1bfc48349cd59c9b
Alexandre Reis <alexmmreis@gmail.com>
2025-12-11 16:16:54 +0000

    save changes

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ var inputMovementsDemo = "L68\r\nL30\r\nR48\r\nL5\r\nR60\r\nL55\r\nL1\r\nL99\r\n
 
 var inputMovements = File.ReadAllText("input-d1-p1.txt");
 
-var movements = inputMovements.Split("\r\n").ToList();
+var movements = inputMovementsDemo.Split("\r\n").ToList();
 
 var timesReachedZero = CalculateTimesReachedOrPassedZero(movements);
 
@@ -20,58 +20,45 @@ int CalculateTimesReachedOrPassedZero(List<string> movements)
 
     foreach (var movement in movements)
     {
-        var loopStartPosition = currentPosition;
-        Console.WriteLine("positionBeforeMovement=" + currentPosition);
-        Console.WriteLine("movement=" + movement);
         var direction = movement[0];
         var nbrOfPositions = int.Parse(movement[1..]);
         var test = "";
         var nbrOfTurns = 0;
-        if (direction != 'L' && direction != 'R')
+
+        if(direction == 'L')
         {
-            test = "test123";
+            nbrOfPositions = nbrOfPositions * -1;
         }
 
-        if (nbrOfPositions < 0 || nbrOfPositions > 99)
+        currentPosition += nbrOfPositions;
+
+
+        if(currentPosition < 0)
         {
-            test = "test123";
+            var negativePositionResult = handleNegativePosition(currentPosition);
+            currentPosition = negativePositionResult.endPosition;
+            timesReachedZero += negativePositionResult.timesPassedOnZero;
         }
 
-        if (direction == 'L')
+        if (currentPosition > 99)
         {
-            //if (nbrOfPositions > currentPosition && nbrOfPositions < 100)
-            //{
-            //    timesReachedZero++;
-            //}
-
-            currentPosition = currentPosition - nbrOfPositions;
-
-        }
-        else
-        {
-            currentPosition = currentPosition + nbrOfPositions;
-        }
-
-        if (currentPosition < 0 || currentPosition > 99)
-        {
-            timesReachedZero += Math.Abs(currentPosition / 100);
-
-            currentPosition = currentPosition % 100;
-
-            if (currentPosition < 0)
-            {
-                currentPosition = currentPosition + 100;
-            }
+            var bigPositionResult = handleBigPosition(currentPosition);
+            currentPosition = bigPositionResult.endPosition;
+            timesReachedZero += bigPositionResult.timesPassedOnZero;
         }
 
         if (currentPosition == 0)
         {
             timesReachedZero++;
         }
-
-        Console.WriteLine("timesReachedZero=" + timesReachedZero);
-        Console.WriteLine("positionAfterMovement=" + currentPosition);
     }
 
     return timesReachedZero;
 }
+
+
+(int endPosition, int timesPassedOnZero) handleNegativePosition(int position) 
+    => ((position % 100) + 100, Math.Abs(position / 100) + 1);
+
+(int endPosition, int timesPassedOnZero) handleBigPosition(int position) 
+    => (position % 100, Math.Abs(position / 100));
